c43e8dad03b3c8b14ee2fed548e0ec279cf1caa5
mhmohammadullahmh <mohammadullah1258@gmail.com>
2023-08-09 14:11:56 +0600

    new

---
+++ 4th_Class/Naming_variable.py
@@ -0,0 +1,23 @@
+#-------------Camel case----------------
+
+""" nameOfStudent  -----[ai varible er e 1st word er sob letter small hbe and porer
+word gulor 1st letter gulo upper case hbe just"""
+
+
+
+
+#------------pascal case-----------------
+
+
+"""NameOfStudnt-------[aitar khetre just sob gula word er 1st letter
+upper case er hbe"""
+
+
+
+#----------------snake case-------------
+
+"""name_of_student--------[aitar khtre sobgula word er majhe just underscore hbe"""
+
+
+
+'''1)Variable er shurute number hbe na'''
---
+++ 4th_Class/object_indentify.py
@@ -0,0 +1,24 @@
+a=50
+b=a
+c=50
+
+
+print(a)
+print(b)
+print(c)
+
+
+
+print(id(a))  #id er maddhone adress ba location print kora jay
+print(id(b))
+print(id(c))
+
+
+
+print(type(a))  #kon type er aita dekhar jnno type use kora hoy
+
+
+#reassigned varible a
+a=400
+
+print(id(a))

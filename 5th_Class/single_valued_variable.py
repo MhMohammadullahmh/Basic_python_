77bb4740ee6244b8aeaa4ec34feb2fe7ad1856a4
mhmohammadullahmh <mohammadullah1258@gmail.com>
2023-08-09 23:51:32 +0600

    new

---
+++ 5th_Class/single_valued_variable.py
@@ -0,0 +1,16 @@
+#=-------------printing single valued variable-------
+
+a=5
+print(a)
+print((a))
+
+
+#printing multiple variables
+
+b=6
+c=54
+print(c,b)
+
+#separate the value by comma
+
+print(3,5,65,3,5,3,54,23,45)

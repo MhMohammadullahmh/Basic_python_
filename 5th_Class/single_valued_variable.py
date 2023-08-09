1d480eb52a3c62b04e50f325cefaa237dc1d9940
mhmohammadullahmh <mohammadullah1258@gmail.com>
2023-08-10 00:59:54 +0600

    new

--- 5th_Class/single_valued_variable.py
+++ 5th_Class/single_valued_variable.py
@@ -14,3 +14,18 @@ print(c,b)
 #separate the value by comma
 
 print(3,5,65,3,5,3,54,23,45)
+
+
+#---------------------Boolean literals-------------------------
+#true mane 1 & False mane 0
+
+x=True
+y=False
+a=True+4   #akhane true mane 1 dhore nise tai result ashbe 5
+b=False+6   #akhane false mane o dhore nise tai result ashbe 6 e
+
+print("x is",x)
+print("y is",y)
+print("a:",a)
+print("b:",b)
+print(type(x))
---
+++ 5th_Class/string_print.py
@@ -0,0 +1,15 @@
+#printing string variable
+
+my_string="This is python"
+this_char="C"
+multiline_str="""This is a multiline
+string with more than one line"""  #multiline stringer khetre 3 ta double coatation dite hoy
+
+unicode_str=u"\u00dcnic\u00f6de"   #dekhte hbe ?????
+raw_str=r"raw\n string"
+
+print(my_string)
+print(this_char)
+print(multiline_str)
+print(unicode_str)
+print(raw_str)
